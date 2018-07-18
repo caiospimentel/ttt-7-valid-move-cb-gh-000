9e36ccd6150fbce4c52e3df40c36cd403dcf56f7
CAIO PIMENTEL <caio3191@gmail.com>
2018-07-18 15:05:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,7 @@ def valid_move? (board, index)
     return false
   else 
     return position_taken
+  end
 end
 
 def position_taken?
@@ -10,5 +11,6 @@ def position_taken?
     return false
   else
     return true
+  end
   
 end
